4c48ae4964e6663c5312a29a871488d5e3dee63b
ErnestoCarcamo <car.ernestodavid@gmail.com>
2020-10-15 20:40:31 -0600

    cambio de puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //importar
 const express = require("express");
 const app = express;
-app.listen(5500,()=>{
-    console.log("Servidor ejecutandose, puerto 5500")
+app.listen(9500,()=>{
+    console.log("Servidor ejecutandose, puerto 9500")
 });
